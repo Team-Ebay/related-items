a02034cbcde185e1e1fc28b49e8f67a25eceeda5
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-23 14:21:29 -0700

    Uses fetch request to get data from database and pass down as props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import ProductCarousel from './component/ProductCarousel';
 
 const App = () => {
+
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts({data}))
+  })
+
   return (
     <>
+    <ProductCarousel products={products} />
     </>
   );
 }
