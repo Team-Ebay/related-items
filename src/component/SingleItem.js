a91236d3348d170ead733c41db4d026d74f9d1ba
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-24 13:09:45 -0700

    Changes classnames of content for styling

--- src/component/SingleItem.js
+++ src/component/SingleItem.js
@@ -2,15 +2,18 @@ const SingleItem = (props) => {
 
     return (
        
-        <div className="singleItem">
+        <div className="cardContainer">
 
+            <div className="card">
+            <div className="image">
             <img className="backImage" src={props.elem.image_url} alt='none' />
+            </div>
             <p className="itemName">{props.elem.name}</p>            
             <p className="itemDesc">{props.elem.condition}</p>
             <h2 className="price">{props.elem.price}</h2>
             <p className="itemDesc">{props.elem.shipping}</p>
-            <p className="itemDesc">{props.elem.rating}</p>
             <h3 className="itemDesc">{props.elem.items_sold}</h3>
+            </div>
 
         </div>
     )
