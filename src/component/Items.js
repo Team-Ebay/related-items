ec6aac41ecec58b24f5f43d62fa92f355eb0b449
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-23 21:48:57 -0700

    Creates buttons and header

--- src/component/Items.js
+++ src/component/Items.js
@@ -1,9 +1,19 @@
 import SingleItem from "./SingleItem";
 
 const Items = (props) => {
-    props.props.products.data.map((elem) => {
-        return <SingleItem elem={elem} key={elem.product_id} />
-    })
+    
+    return (
+
+    <div className="carousel">
+        <button aria-label="Previous slide" class="carousel__previous" type="button"></button>
+    <h2 className="header">Sponsored items based on your recent views</h2>
+    {props.products.map((elem) => {
+        return (
+            <SingleItem elem={elem} key={elem.product_id} />
+    )})}
+        <button aria-label="Next slide" class="carousel__next" type="button"></button>
+    </div>
+    )
 }
 
 export default Items;
