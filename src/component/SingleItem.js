99b150884912859634608fad449e33e6bf5cd2e8
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-23 21:49:10 -0700

    Gives elements class names for styling

--- src/component/SingleItem.js
+++ src/component/SingleItem.js
@@ -1,5 +1,19 @@
-const SingleItem = () => {
+const SingleItem = (props) => {
 
+    return (
+       
+        <div className="singleItem">
+
+            <img className="backImage" src={props.elem.image_url} alt='none' />
+            <p className="itemName">{props.elem.name}</p>            
+            <p className="itemDesc">{props.elem.condition}</p>
+            <h2 className="price">{props.elem.price}</h2>
+            <p className="itemDesc">{props.elem.shipping}</p>
+            <p className="itemDesc">{props.elem.rating}</p>
+            <h3 className="itemDesc">{props.elem.items_sold}</h3>
+
+        </div>
+    )
 }
 
 export default SingleItem
