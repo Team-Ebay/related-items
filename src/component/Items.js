4775a918cece414f68acde3ff44d6c336367dd13
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-24 13:09:34 -0700

    Changs order of content

--- src/component/Items.js
+++ src/component/Items.js
@@ -1,17 +1,36 @@
 import SingleItem from "./SingleItem";
 
+
 const Items = (props) => {
+
+    const carouselItems = document.querySelector('.carouselItems')
     
+    const scroll = (scrollOffset) => {
+        carouselItems.scrollLeft += scrollOffset;
+    }
+
     return (
 
     <div className="carousel">
-        <button aria-label="Previous slide" class="carousel__previous" type="button"></button>
+       
     <h2 className="header">Sponsored items based on your recent views</h2>
-    {props.products.map((elem) => {
-        return (
-            <SingleItem elem={elem} key={elem.product_id} />
-    )})}
-        <button aria-label="Next slide" class="carousel__next" type="button"></button>
+    <button onClick={() => scroll(-1200)}>
+        <span className="material-icons">
+            chevron_left
+            </span>
+        </button>
+    <div className="carouselItems">
+        {props.products.map((elem) => {
+            return (
+                <SingleItem elem={elem} key={elem.product_id} />
+        )})}
+
+    </div>
+        <button onClick={() => scroll(1150)}>
+            <span className="material-icons">
+            chevron_right
+            </span>
+        </button>
     </div>
     )
 }
